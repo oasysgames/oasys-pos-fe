da63a03b18983c0cc3c01595ed8c6b83ecf1924e
Tsuyoshi-Ishikawa <tsuyozo7@gmail.com>
2022-10-15 23:26:50 +0900

    set default contract address

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,1 +1,1 @@
-export const contractAddress = process.env.NEXT_PUBLIC_CONTRACT_ADDRESS ?? '';
+export const contractAddress = process.env.NEXT_PUBLIC_CONTRACT_ADDRESS ?? '0x0000000000000000000000000000000000001001';
