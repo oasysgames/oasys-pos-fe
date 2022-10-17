730a2d7efc8e1550a9f0eeb1b5d9ebafd8776760
Tsuyoshi-Ishikawa <tsuyozo7@gmail.com>
2022-10-17 14:36:48 +0900

    check already operator set

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,27 +1,38 @@
 import type { NextPage } from 'next';
 import styles from '../styles/Home.module.css';
-import { useState, useRef } from 'react';
+import { useState } from 'react';
 import { ethers } from 'ethers';
 import StakeManager from '../contracts/StakeManager.json';
 import { contractAddress } from '../config';
 import { getSigner } from '../features';
 
 const Home: NextPage = () => {
-  let signer: ethers.providers.JsonRpcSigner
+  const [ownerAddressError, setOwnerAddressError] = useState('');
   const [ownerAddress, setOwnerAddress] = useState('');
   const [operatorAddressError, setOperatorAddressError] = useState('');
   const [operatorAddress, setOperatorAddress] = useState('');
 
   const setOwner = async () => {
-    signer = await getSigner();
+    const signer = await getSigner();
     const address = await signer.getAddress();
+    const contract = new ethers.Contract(contractAddress, StakeManager.abi, signer);
+
     setOwnerAddress(address);
+    try {
+      const result = await contract.getValidatorInfo(address, 0);
+      if (result.operator !== '0x0000000000000000000000000000000000000000' && result.candidate) {
+        setOperatorAddress(result.operator);
+      }
+    } catch (err) {
+      if (err instanceof Error) {
+        setOwnerAddressError(err.message);
+      }
+    }
   }
   const Submit = async () => {
-    if (!signer) {
-      signer = await getSigner();
-    }
+    const signer = await getSigner();
     const contract = new ethers.Contract(contractAddress, StakeManager.abi, signer);
+
     try {
       await contract.joinValidator(operatorAddress);
     } catch (err) {
@@ -33,6 +44,9 @@ const Home: NextPage = () => {
   return (
     <div>
       <div>
+        {ownerAddressError && (
+          <p>{ ownerAddressError }</p>
+        )}
         <p>Owner Address:  { ownerAddress}</p>
         <button onClick={setOwner}>Set owner  by metamask</button>
       </div>
