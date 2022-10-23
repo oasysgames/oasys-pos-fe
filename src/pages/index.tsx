66da8e649643ecdbcaaae3b79acb106ef231aa3b
Tsuyoshi-Ishikawa <tsuyozo7@gmail.com>
2022-10-23 17:13:30 +0900

    check allowList before set opearator

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,7 +2,8 @@ import type { NextPage } from 'next';
 import { useState } from 'react';
 import { ethers } from 'ethers';
 import StakeManager from '../contracts/StakeManager.json';
-import { contractAddress } from '../config';
+import AllowList from '../contracts/AllowList.json';
+import { stakeManagerAddress, allowListAddress } from '../config';
 import { getSigner } from '../features';
 import { Button, Input, ErrorMsg, SuccessMsg } from '../components';
 
@@ -14,14 +15,24 @@ const Home: NextPage = () => {
   const [newOperator, setNewOperator] = useState('');
   const [successMsg, setSuccessMsg] = useState('');
 
+  const refreshError = () => {
+    setOwnerAddressError('');
+    setOperatorAddressError('');
+  };
+
   const setOwner = async () => {
     const signer = await getSigner();
     const address = await signer.getAddress();
-    const contract = new ethers.Contract(contractAddress, StakeManager.abi, signer);
+    const stakeManagerContract = new ethers.Contract(stakeManagerAddress, StakeManager.abi, signer);
+    const allowListContract = new ethers.Contract(allowListAddress, AllowList.abi, signer);
 
     setOwnerAddress(address);
     try {
-      const result = await contract.getValidatorInfo(address, 0);
+      const iaAllow = await allowListContract.containsAddress(address);
+      if (!iaAllow) {
+        throw new Error('owner is not allowed to be validator');
+      }
+      const result = await stakeManagerContract.getValidatorInfo(address, 0);
       if (result.operator !== '0x0000000000000000000000000000000000000000') {
         setOperatorAddress(result.operator);
       }
@@ -31,40 +42,53 @@ const Home: NextPage = () => {
       }
     }
   }
+
   const registerOperator = async () => {
     const signer = await getSigner();
-    const contract = new ethers.Contract(contractAddress, StakeManager.abi, signer);
+    const stakeManagerContract = new ethers.Contract(stakeManagerAddress, StakeManager.abi, signer);
+    const allowListContract = new ethers.Contract(allowListAddress, AllowList.abi, signer);
 
     try {
-      await contract.joinValidator(newOperator);
+      const iaAllow = await allowListContract.containsAddress(ownerAddress);
+      if (!iaAllow) {
+        throw new Error('owner is not allowed to be validator');
+      }
+      await stakeManagerContract.joinValidator(newOperator);
       setOperatorAddress(newOperator);
       setNewOperator('');
-      setOwnerAddressError('');
-      setOperatorAddressError('');
+      refreshError();
       setSuccessMsg('operator register is successful');
     } catch (err) {
       if (err instanceof Error) {
         setOperatorAddressError(err.message);
+        setSuccessMsg('');
       }
     }
   }
+
   const updateOperator = async () => {
     const signer = await getSigner();
-    const contract = new ethers.Contract(contractAddress, StakeManager.abi, signer);
+    const stakeManagerContract = new ethers.Contract(stakeManagerAddress, StakeManager.abi, signer);
+    const allowListContract = new ethers.Contract(allowListAddress, AllowList.abi, signer);
 
     try {
-      await contract.updateOperator(newOperator);
+      const iaAllow = await allowListContract.containsAddress(ownerAddress);
+      if (!iaAllow) {
+        throw new Error('owner is not allowed to be validator');
+      }
+      await stakeManagerContract.updateOperator(newOperator);
       setOperatorAddress(newOperator);
       setNewOperator('');
-      setOwnerAddressError('');
-      setOperatorAddressError('');
+      refreshError();
       setSuccessMsg('operator update is successful');
     } catch (err) {
       if (err instanceof Error) {
         setOperatorAddressError(err.message);
+        setSuccessMsg('');
       }
     }
   }
+
   return (
     <div className='px-2 py-2'>
       <div className='space-y-4'>
