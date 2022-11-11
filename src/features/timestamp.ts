3b87c9ee0b067db490642332afc266099bc87143
Tsuyoshi-Ishikawa <tsuyozo7@gmail.com>
2022-11-11 13:25:14 +0900

    modify formattedDate

--- src/features/timestamp.ts
+++ src/features/timestamp.ts
@@ -1,11 +1,3 @@
 export const formattedDate = (date: Date) => {
-  const formattedDate = `
-    ${date.getMonth() + 1}/${date.getDate()}/${date.getFullYear()},
-    ${date.getHours()}:${formattedMinutes(date.getMinutes())}
-  `;
-  return formattedDate;
-};
-
-const formattedMinutes = (minutes: number) => {
-  return String(minutes).padStart(2, '0');
+  return `${date.toLocaleDateString()} ${date.toLocaleTimeString()}`;
 };
