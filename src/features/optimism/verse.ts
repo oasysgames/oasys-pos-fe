df4c9e6c014e3557455947d6e52c12c6e9491f0f
ironbeer <7997273+ironbeer@users.noreply.github.com>
2023-02-12 18:20:58 +0900

    Changed OVM_ETH to OVM_OAS in Layer2 genesis

--- src/features/optimism/verse.ts
+++ src/features/optimism/verse.ts
@@ -1,12 +1,36 @@
-import { getNamedAddresses, getSigner } from '@/features';
-import { GenesisParams } from '@/types/optimism/genesis';
-import { GenesisGasParams, GenesisBlockParams, GenesisCliqueParams, ZERO_ADDRESS, MAINNET_CHAIN_ID, Mainnet_OvmBlockSignerAddress, Testnet_OvmBlockSignerAddress } from '@/consts';
-import { makeGenesisJson } from '@/features/optimism';
-import { VerseInfo } from '@/types/optimism/verse';
+import { BigNumber } from "ethers";
 
-export const getVerseInfo = async (builder: string, verseChainId: number): Promise<VerseInfo> => {
+import {
+  getNamedAddresses,
+  getSigner,
+  getL1BuildDepositContract,
+} from "@/features";
+import { GenesisParams } from "@/types/optimism/genesis";
+import {
+  GenesisGasParams,
+  GenesisBlockParams,
+  GenesisCliqueParams,
+  ZERO_ADDRESS,
+  MAINNET_CHAIN_ID,
+  Mainnet_OvmBlockSignerAddress,
+  Testnet_OvmBlockSignerAddress,
+} from "@/consts";
+import { makeGenesisJson } from "@/features/optimism";
+import { VerseInfo } from "@/types/optimism/verse";
+
+// Use OVM_OAS if created after this block.
+const OVM_OAS_BLOCK = BigNumber.from(763240);
+
+export const getVerseInfo = async (
+  builder: string,
+  verseChainId: number
+): Promise<VerseInfo> => {
   const signer = await getSigner();
   const chainId = await signer.getChainId();
+
+  // Get the block number the verse was built.
+  const l1BuildDeposit = await getL1BuildDepositContract();
+  const block = (await l1BuildDeposit.getBuildBlock(builder)) as BigNumber;
 
   const namedAddresses = await getNamedAddresses(verseChainId);
   const genesisParams: GenesisParams = {
@@ -14,7 +38,11 @@ export const getVerseInfo = async (builder: string, verseChainId: number): Promi
     ovmWhitelistOwner: ZERO_ADDRESS,
     ovmGasPriceOracleOwner: builder,
     ovmFeeWalletAddress: builder,
-    ovmBlockSignerAddress: chainId === MAINNET_CHAIN_ID ? Mainnet_OvmBlockSignerAddress: Testnet_OvmBlockSignerAddress,
+    ovmBlockSignerAddress:
+      chainId === MAINNET_CHAIN_ID
+        ? Mainnet_OvmBlockSignerAddress
+        : Testnet_OvmBlockSignerAddress,
+    useOvmOas: block.gte(OVM_OAS_BLOCK),
     ...GenesisGasParams,
     ...GenesisBlockParams,
     ...GenesisCliqueParams,
@@ -24,5 +52,5 @@ export const getVerseInfo = async (builder: string, verseChainId: number): Promi
     chainId: verseChainId,
     namedAddresses,
     genesis,
-  }
+  };
 };
