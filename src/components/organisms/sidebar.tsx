ad347a247d40ba6277e83d889102c2fa07647e35
ironbeer <7997273+ironbeer@users.noreply.github.com>
2023-06-10 12:15:49 +0900

    Get Verse Info by Chain ID or Builder Address

--- src/components/organisms/sidebar.tsx
+++ src/components/organisms/sidebar.tsx
@@ -1,6 +1,6 @@
 import clsx from 'clsx';
 import { useRouter } from 'next/router';
-import Image from 'next/image'
+import Image from 'next/image';
 import { LinkTab } from '@/components/atoms';
 
 type Props = {
@@ -25,26 +25,21 @@ const Links = [
     url: '/',
   },
   {
-    text: 'Check verse info',
+    text: 'Check verse',
     url: '/check-verse',
-  }
+  },
 ];
 
 export const Sidebar = (props: Props) => {
   const router = useRouter();
-  const {
-    className,
-  } = props;
+  const { className } = props;
 
   return (
-    <div className={clsx(
-      className,
-      'bg-green-500',
-    )}>
+    <div className={clsx(className, 'bg-green-500')}>
       <div className='mt-2 md:mt-4 space-y-10'>
         <Image
-          src="/images/oasys.png"
-          alt="Oasys logo"
+          src='/images/oasys.png'
+          alt='Oasys logo'
           width={250}
           height={250}
         />
