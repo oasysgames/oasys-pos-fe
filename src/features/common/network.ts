fefae48b22579e7d02f8d26dfe79a6eb2fb967e8
Tsuyoshi-Ishikawa <tsuyozo7@gmail.com>
2023-06-19 19:44:52 +0900

    set unknown as not_oasys_network

--- src/features/common/network.ts
+++ src/features/common/network.ts
@@ -3,5 +3,5 @@ import { MAINNET_CHAIN_ID, TESTNET_CHAIN_ID } from "@/consts";
 export const getNetworkName = (chainId: number) => {
   if (chainId === MAINNET_CHAIN_ID) return "Oasys mainnet";
   if (chainId === TESTNET_CHAIN_ID) return "Oasys testnet";
-  return "Network not Oasys";
+  return "Unknown";
 }
