ba9264ab2ca031e453d180b707e8d7e3dc81a7e7
ironbeer <7997273+ironbeer@users.noreply.github.com>
2023-12-19 15:29:33 +0900

    Fixed bug in SOAS and LOAS page for Claimable amount

--- src/hooks/useSOASClaimInfo.ts
+++ src/hooks/useSOASClaimInfo.ts
@@ -12,23 +12,30 @@ const getSOASClaimInfo = async () => {
   if (accounts.length === 0) return undefined;
 
   const signer = await getSigner();
-  const ownerAddress = await signer.getAddress();
   const sOASContract = await getSOASContract();
-  const res = await sOASContract.claimInfo(ownerAddress);
-  const claimable = await sOASContract.getClaimableOAS(ownerAddress);
-  const balance = await sOASContract.balanceOf(ownerAddress);
-  const currentClaimable = claimable.sub(res.claimed);
+
+  const allowedClaimer = await signer.getAddress();
+  const originalClaimer = await sOASContract.originalClaimer(allowedClaimer);
+
+  const balance = await sOASContract.balanceOf(allowedClaimer);
+  const claimInfo = await sOASContract.claimInfo(originalClaimer);
+  const claimable = await sOASContract.getClaimableOAS(originalClaimer);
+
+  let currentClaimable = claimable.sub(claimInfo.claimed);
+  if (balance.lte(currentClaimable)) {
+    currentClaimable = balance;
+  }
 
   // We actually have to toString since and until because since and until are uint64 at solidity.
   // But when mint, since and until are set by javascript.
-  // That's why It is ok that res.since.toNumber() and res.until.toNumber().
+  // That's why It is ok that claimInfo.since.toNumber() and claimInfo.until.toNumber().
   const data: ClaimInfo = {
-    amount: res.amount,
-    claimed: res.claimed,
-    claimable: currentClaimable.gte(balance) ? balance :currentClaimable,
-    since: new Date(res.since.toNumber() * 1000), // res.since unit is seconds
-    until: new Date(res.until.toNumber() * 1000), // res.until unit is seconds
-    from: res.from,
+    amount: balance,
+    claimed: claimInfo.claimed,
+    claimable: currentClaimable,
+    since: new Date(claimInfo.since.toNumber() * 1000), // claimInfo.since unit is seconds
+    until: new Date(claimInfo.until.toNumber() * 1000), // claimInfo.until unit is seconds
+    from: claimInfo.from,
   };
   return data;
 };
@@ -42,7 +49,7 @@ export const useSOASClaimInfo = () => {
     isClaimInfoLoading: !error && !data,
     claimInfoError: error,
   };
-}
+};
 
 export const useRefreshSOASClaimInfo = () => {
   const { mutate } = useSWRConfig();
