5328fd6deab33283844c5d587f047e44f6f6164d
tak <re795h@gmail.com>
2024-03-09 13:18:37 +0700

    fmt

--- src/components/organisms/verseInfoV2.tsx
+++ src/components/organisms/verseInfoV2.tsx
@@ -2,7 +2,10 @@ import clsx from 'clsx';
 import { ErrorMsg, Button, Table } from '@/components/atoms';
 import { useCallback, useState } from 'react';
 import { download, handleError } from '@/features';
-import { VerseInfoV2 as VerseInfoType, DeployConfig } from '@/types/optimism/verse';
+import {
+  VerseInfoV2 as VerseInfoType,
+  DeployConfig,
+} from '@/types/optimism/verse';
 import { NamedAddressesV2 as NamedAddresses } from '@/types/oasysHub/verseBuild';
 
 type Props = {
@@ -11,13 +14,8 @@ type Props = {
   verseInfo: VerseInfoType;
 };
 
-
 export const VerseInfoV2 = (props: Props) => {
-  const {
-    className,
-    verseBuilder,
-    verseInfo,
-  } = props;
+  const { className, verseBuilder, verseInfo } = props;
 
   const [downloadError, setDownloadError] = useState('');
 
@@ -37,108 +35,53 @@ export const VerseInfoV2 = (props: Props) => {
     }
   }, []);
 
-  const heads = [
-    'Config',
-    'Value'
-  ];
+  const heads = ['Config', 'Value'];
 
   const records = [
-    [
-      'Chain_id',
-      verseInfo.chainId.toString(),
-    ],
-    [
-      'Builder',
-      verseBuilder,
-    ],
-    [
-      'FinalSystemOwner',
-      verseInfo.namedAddresses.FinalSystemOwner,
-    ],
-    [
-      'L2OutputOracleProposer',
-      verseInfo.namedAddresses.L2OutputOracleProposer,
-    ],
+    ['Chain_id', verseInfo.chainId.toString()],
+    ['Builder', verseBuilder],
+    ['FinalSystemOwner', verseInfo.namedAddresses.FinalSystemOwner],
+    ['L2OutputOracleProposer', verseInfo.namedAddresses.L2OutputOracleProposer],
     [
       'L2OutputOracleChallenger',
       verseInfo.namedAddresses.L2OutputOracleChallenger,
     ],
-    [
-      'BatchSender',
-      verseInfo.namedAddresses.BatchSender,
-    ],
-    [
-      'ProxyAdmin',
-      verseInfo.namedAddresses.ProxyAdmin,
-    ],
-    [
-      'SystemConfigProxy',
-      verseInfo.namedAddresses.SystemConfigProxy,
-    ],
-    [
-      'L1StandardBridgeProxy',
-      verseInfo.namedAddresses.L1StandardBridgeProxy,
-    ],
-    [
-      'L1ERC721BridgeProxy',
-      verseInfo.namedAddresses.L1ERC721BridgeProxy,
-    ],
+    ['BatchSender', verseInfo.namedAddresses.BatchSender],
+    ['ProxyAdmin', verseInfo.namedAddresses.ProxyAdmin],
+    ['SystemConfigProxy', verseInfo.namedAddresses.SystemConfigProxy],
+    ['L1StandardBridgeProxy', verseInfo.namedAddresses.L1StandardBridgeProxy],
+    ['L1ERC721BridgeProxy', verseInfo.namedAddresses.L1ERC721BridgeProxy],
     [
       'L1CrossDomainMessengerProxy',
       verseInfo.namedAddresses.L1CrossDomainMessengerProxy,
     ],
-    [
-      'L2OutputOracleProxy',
-      verseInfo.namedAddresses.L2OutputOracleProxy,
-    ],
-    [
-      'OptimismPortalProxy',
-      verseInfo.namedAddresses.OptimismPortalProxy,
-    ],
-    [
-      'ProtocolVersions',
-      verseInfo.namedAddresses.ProtocolVersions,
-    ],
-    [
-      'BatchInbox',
-      verseInfo.namedAddresses.BatchInbox,
-    ],
-    [
-      'AddressManager',
-      verseInfo.namedAddresses.AddressManager,
-    ],
-    [
-      'P2PSequencer',
-      verseInfo.namedAddresses.P2PSequencer,
-    ],
+    ['L2OutputOracleProxy', verseInfo.namedAddresses.L2OutputOracleProxy],
+    ['OptimismPortalProxy', verseInfo.namedAddresses.OptimismPortalProxy],
+    ['ProtocolVersions', verseInfo.namedAddresses.ProtocolVersions],
+    ['BatchInbox', verseInfo.namedAddresses.BatchInbox],
+    ['AddressManager', verseInfo.namedAddresses.AddressManager],
+    ['P2PSequencer', verseInfo.namedAddresses.P2PSequencer],
   ];
 
   return (
-    <div className={clsx(
-      className,
-    )}>
-      {downloadError && (
-          <ErrorMsg text={ downloadError } className='w-full' />
-        )}
-        <p>Download verse config</p>
-        <Table
-          heads={heads}
-          records={records}
-        />
-          {verseInfo?.namedAddresses && verseInfo?.deployConfig &&
-            <div className="flex flex-col space-y-2">
-              <Button
-                handleClick={() => downloadAddresses(verseInfo.namedAddresses)}
-              >
-                Download address.json
-              </Button>
-              <Button
-                handleClick={() => downloadDeployConfig(verseInfo.deployConfig)}
-              >
-                Download deploy-config.json
-              </Button>
-            </div>
-          }
+    <div className={clsx(className)}>
+      {downloadError && <ErrorMsg text={downloadError} className='w-full' />}
+      <p>Download verse config</p>
+      <Table heads={heads} records={records} />
+      {verseInfo?.namedAddresses && verseInfo?.deployConfig && (
+        <div className='flex flex-col space-y-2'>
+          <Button
+            handleClick={() => downloadAddresses(verseInfo.namedAddresses)}
+          >
+            Download address.json
+          </Button>
+          <Button
+            handleClick={() => downloadDeployConfig(verseInfo.deployConfig)}
+          >
+            Download deploy-config.json
+          </Button>
+        </div>
+      )}
     </div>
   );
 };
